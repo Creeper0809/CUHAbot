17014050983e4c6ec2253556cfa2da0007b410bb
sijogom <sijogom1105@gmail.com>
2025-05-13 15:37:38 +0900

    장비 아이템 - 공격력, 체력, 속도, 등급, 장착 위치, 가격 소모 아이템 - HP회복량, 가격 기타 아이템 - 가격 (따로 지정 x, 가격만 표시)
    스킬 - 만들 예정

--- models/item.py
+++ models/item.py
@@ -3,7 +3,7 @@ from tortoise.fields import ReverseRelation
 from enum import Enum
 
 # 아이템 타입 정의
-class ItemType(Enum):
+class ItemType(str, Enum):
     CONSUME = 'consume'
     EQUIP = 'equip'
     SKILL = 'skill'
@@ -17,12 +17,13 @@ class Item(Model):
     image_link = fields.TextField(null=True)
     cost = fields.IntField()
     emoji_id = fields.TextField(null=True)
-    type = fields.CharEnumField(enum_type=ItemType, null=True)
+    type = fields.CharEnumField(ItemType, null=True)
 
     class Meta:
         table = "item"
 
     equipment_item = ReverseRelation["EquipmentItem"]
+    consume_item = ReverseRelation["ConsumeItem"]
 
 # 장비 아이템 정보 추가
     async def create_equipment(self, equipment_data):
@@ -32,14 +33,25 @@ class Item(Model):
                 item=self,
                 attack=equipment_data.get('attack'),
                 hp=equipment_data.get('hp'),
-                ap_attack=equipment_data.get('ap_attack'),
-                ad_defanse=equipment_data.get('ad_defanse'),
-                ap_defanse=equipment_data.get('ap_defanse'),
+                speed=equipment_data.get('speed'),
                 grade=equipment_data.get('grade'),
                 equip_pos=equipment_data.get('equip_pos')
             )
             return equipment
         return None
+
+# 소비 아이템 정보 추가
+    async def create_consume(self, consume_data):
+        if self.type == ItemType.CONSUME:
+            from models.consume_item import ConsumeItem
+            consume = await ConsumeItem.create(
+                item=self,
+                consume_id=self.id,
+                amount=consume_data.get('amount')
+            )
+            return consume
+        return None
+
 
     def __str__(self):
         return self.name or str(self.id)
