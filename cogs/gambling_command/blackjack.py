a5a9a3eaedf238897e90d280bd6f3e314625b197
Adelio <2024011862@office.cju.ac.kr>
2025-05-17 00:44:35 +0900

    .

--- cogs/gambling_command/blackjack.py
+++ cogs/gambling_command/blackjack.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from discord.ext import commands
+from discord.ui import Button, View
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,4 +15,16 @@ def create_card(self):
             '8', '9', '10', 'J', 'Q', 'K']
     return[f"{cards}{special_card}"]
 
-    
+def card_value(card):
+    rank = card[:0]
+    if card in ['J', 'Q', 'K']:
+        return 10
+    elif card == 'A':
+        return 11
+    else:
+        return int(rank)
+
+class blackjack(View):
+    def __init__(self, ctx):
+        super().__init__(timeout = 120)
+        self.ctx = ctx
