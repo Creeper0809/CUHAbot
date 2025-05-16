220ea322ea397b8a30095acec87c5f244378c1a5
Adelio <2024011862@office.cju.ac.kr>
2025-05-17 00:40:54 +0900

    갈아 엎는 중

--- cogs/gambling_command/RockScissorPaper.py
+++ cogs/gambling_command/RockScissorPaper.py
@@ -1,9 +1,27 @@
 import discord
 import random
 from discord.ext import commands
+from discord.ui import Button, View
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
+
+def result(user, bot):
+    if user == bot:
+        return "무승부"
+    elif (user == "가위" and bot == "보") or \
+         (user == "바위" and bot == "가위") or \
+         (user == "보" and bot == "바위"):
+        return "승리"
+    else:
+        return "패배"
+
+class ButtonView(View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(Button(label="가위", style=discord.ButtonStyle.green))
+        self.add_item(Button(label="바위", style=discord.ButtonStyle.blurple))
+        self.add_item(Button(label="보", style=discord.ButtonStyle.red))
 
 @bot.event
 async def on_ready():
@@ -11,16 +29,16 @@ async def on_ready():
 
 @bot.tree.command(name="가위바위보")
 async def rsp(interaction: discord.Interaction, user_choice: str):
-    choices = ["가위", "바위", "보"]
-    bot_choice = random.choice(choices)
+    await interaction.response.send_message("가위, 바위, 보 중 하나를 선택해주세요.", view=ButtonView())
 
-    if user_choice == bot_choice:
-        result = "무승부"
-    elif (user_choice == "가위" and bot_choice == "보") or (user_choice == "바위" and bot_choice == "가위") or (
-        user_choice == "보" and bot_choice == "바위"):
-        result = "승리"
-    else:
-        result = "패배"
+@bot.event
+async def on_button_click(interaction: discord.Interaction, get_result=None):
+    if interaction.type == discord.InteractionType.component:
+        user_choice_list = { "Rock: 가위", "Paper: 바위", "Scissor: 보" }
+
+        user_choice = user_choice_list.get(interaction.data["custom_id"].split(": "))
+        bot_choice = random.choice(["가위", "바위", "보"])
+        result = get_result(user_choice, bot_choice)
 
     await interaction.response.send_message(f'당신의 선택: {user_choice}\n'
                                             f'쿠하 봇의 선택: {bot_choice}\n'
