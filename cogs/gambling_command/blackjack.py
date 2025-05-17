0ef2ec03c5264523d8c43da803cf4e3e69ee6249
Adelio <2024011862@office.cju.ac.kr>
2025-05-17 22:35:59 +0900

    임베드, 패 업데이트

--- cogs/gambling_command/blackjack.py
+++ cogs/gambling_command/blackjack.py
@@ -36,3 +36,32 @@ class blackjack(View):
         self.my_hand = [sum_list[0] + sum_list[2], f"{selected[0]} | {selected[2]}"]
         self.dealer_hand = [sum_list[1] + sum_list[3], f"{selected[1]} | {selected[3]}"]
         self.message = None
+
+    async def send_embed(self):
+        embed = discord.Embed(title = "Blackjack", color = 0x00ff00)
+        embed.add_field(name = f"{self.ctx.author.name}의 패", value = f"{self.my_hand[1]}\n 합계: {self.my_hand[0]}", inline = False)
+        embed.add_field(name = "총 합", value = f"{self.sum_list[0]} | {self.sum_list[2]}\n{self.sum_list[1]} | {self.sum_list[3]}", inline = False)
+        embed.add_field(name = "딜러의 패", value = f"{self.selected[1]} | ❔", inline = False)
+        embed.add_field(name = f"{self.ctx.author.name}의 차례입니다.", value = "버튼을 눌러주세요.", inline = False)
+
+        if self.message:
+            await self.message.edit(embed = embed, view = self)
+        else:
+            self.message = await self.ctx.send(embed = embed, view = self)
+
+    async def hand_update(self):
+        if self.my_hand[0] == 21 and self.card:
+            self.my_hand[0] = "블랙잭"
+            await self.send_embed()
+            self.stop()
+        elif isinstance(self.my_hand[0], int) and self.my_hand[0] >= 22:
+            if self.my_hand[1].count("A") - self.use_ace >= 1:
+                self.my_hand[0] -= 10
+                self.use_ace += 1
+                await self.send_embed()
+            else:
+                self.my_hand[0] = "버스트"
+                await self.send_embed()
+                self.stop()
+        else:
+            await self.send_embed()
