6f6bb5b5c9bcba3f6d66fbf27ef8c85cfa4b5cad
Adelio <2024011862@office.cju.ac.kr>
2025-05-22 22:10:59 +0900

    무한 갈아엎기

--- cogs/gambling_command/RockScissorPaper.py
+++ cogs/gambling_command/RockScissorPaper.py
@@ -17,29 +17,6 @@ def result(user, bot):
         return "패배"
 
 class ButtonView(View):
-    def __init__(self):
+    def __init__(self, ctx):
         super().__init__()
-        self.add_item(Button(label="가위", style=discord.ButtonStyle.green))
-        self.add_item(Button(label="바위", style=discord.ButtonStyle.blurple))
-        self.add_item(Button(label="보", style=discord.ButtonStyle.red))
-
-@bot.event
-async def on_ready():
-    print("봇 준비 완료")
-
-@bot.tree.command(name="가위바위보")
-async def rsp(interaction: discord.Interaction, user_choice: str):
-    await interaction.response.send_message("가위, 바위, 보 중 하나를 선택해주세요.", view=ButtonView())
-
-@bot.event
-async def on_button_click(interaction: discord.Interaction, get_result=None):
-    if interaction.type == discord.InteractionType.component:
-        user_choice_list = { "Rock: 가위", "Paper: 바위", "Scissor: 보" }
-
-        user_choice = user_choice_list.get(interaction.data["custom_id"].split(": "))
-        bot_choice = random.choice(["가위", "바위", "보"])
-        result = get_result(user_choice, bot_choice)
-
-    await interaction.response.send_message(f'당신의 선택: {user_choice}\n'
-                                            f'쿠하 봇의 선택: {bot_choice}\n'
-                                            f'결과: {result}')
+        self.ctx = ctx
